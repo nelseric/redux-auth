c73bed0c70bf055ae09031e07a89f6e62e1a43d4
Eric Nelson <nelseric.alt@gmail.com>
2016-02-03 19:04:09 -0600

    Dev Build

--- webpack.release.js
+++ webpack.release.js
@@ -5,7 +5,7 @@ module.exports = {
   target:  "web",
   cache:   false,
   context: __dirname,
-  devtool: false,
+  devtool: "#cheap-module-eval-source-map",
   entry:   {
     "index":             "./src/index",
     "bootstrap-theme":   "./src/views/bootstrap/index",
@@ -56,7 +56,7 @@ module.exports = {
     new webpack.DefinePlugin({"process.env": {NODE_ENV: "\"production\""}}),
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin()
+    // new webpack.optimize.UglifyJsPlugin()
   ],
   module:  {
     loaders: [
